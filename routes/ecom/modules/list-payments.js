af943f835438a72c36d4ca1282e8336d66d5c5db
Leonardo Matos <leomp120894@gmail.com>
2019-06-21 12:33:33 -0300

    fix: fix treating module request body on list payments endpoint

--- routes/ecom/modules/list-payments.js
+++ routes/ecom/modules/list-payments.js
@@ -1,55 +1,62 @@
 'use strict'
 
-// read configured E-Com Plus app data
-const getConfig = require(process.cwd() + '/lib/store-api/get-config')
 // generate default payment gateway object
 const newPaymentGateway = require(process.cwd() + '/lib/new-payment-gateway')
 
 module.exports = appSdk => {
   return (req, res) => {
-    const { storeId, body } = req
-    // get app configured options
-    getConfig({ appSdk, storeId })
+    const { body } = req
+    if (!body) {
+      return res.sendStatus(400)
+    }
 
-      .then(config => {
-        // treat list payments module request body and mount response
-        // https://apx-mods.e-com.plus/api/v1/list_payments/schema.json?store_id=100
-        // https://apx-mods.e-com.plus/api/v1/list_payments/response_schema.json?store_id=100
-        let paymentGateway = newPaymentGateway(body.lang)
-        // merge cunfigured options to payment gateway object
-        ;[ 'label', 'text', 'icon', 'discount' ].forEach(prop => {
-          if (config[prop]) {
-            paymentGateway[prop] = config[prop]
-          }
-        })
-        // setup response object
-        let response = {
-          payment_gateways: [ paymentGateway ]
-        }
-
-        if (paymentGateway.discount) {
-          // default discount option
-          let { type, value } = paymentGateway
-          let label = config.discount_option_label || paymentGateway.label
-          response.discount_option = { label, value }
-          // specify the discount type is optional
-          if (type) {
-            response.discount_option.type = type
-          }
-        }
-
-        res.send(response)
+    // treat module request body
+    const { params, application } = body
+    if (!params || !application) {
+      return res.sendStatus(400)
+    }
+    // app configured options
+    const config = Object.assign({}, application.data, application.hidden_data)
+    if (!config.paghiper_api_key) {
+      // must have configured PagHiper API key and token
+      return res.status(406).send({
+        error: 'LIST_PAYMENTS_ERR',
+        message: 'PagHiper API key is unset on app hidden data (merchant must configure the app)'
       })
+    }
 
-      .catch(err => {
-        // request to Store API with error response
-        // return error status code
-        res.status(500)
-        let { message } = err
-        res.send({
-          error: 'LIST_PAYMENTS_ERR',
-          message
-        })
-      })
+    // params object follows list payments request schema:
+    // https://apx-mods.e-com.plus/api/v1/list_payments/schema.json?store_id=100
+    if (!params.lang) {
+      // set PT-BR as default
+      params.lang = 'pt_br'
+    }
+
+    // start mounting response body
+    // https://apx-mods.e-com.plus/api/v1/list_payments/response_schema.json?store_id=100
+    let paymentGateway = newPaymentGateway(params.lang)
+    // merge cunfigured options to payment gateway object
+    ;[ 'label', 'text', 'icon', 'discount' ].forEach(prop => {
+      if (config[prop]) {
+        paymentGateway[prop] = config[prop]
+      }
+    })
+    // setup response object
+    let response = {
+      payment_gateways: [ paymentGateway ]
+    }
+
+    if (paymentGateway.discount) {
+      // default discount option
+      let { type, value } = paymentGateway
+      let label = config.discount_option_label || paymentGateway.label
+      response.discount_option = { label, value }
+      // specify the discount type is optional
+      if (type) {
+        response.discount_option.type = type
+      }
+    }
+
+    res.send(response)
   }
 }
