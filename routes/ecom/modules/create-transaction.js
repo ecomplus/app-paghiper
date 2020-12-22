385badfe559133099237d21db9067d93ef431a1c
Leonardo Matos <leomp120894@gmail.com>
2020-12-22 17:50:55 -0300

    fix(create-transaction): try many pix url for payment link from paghiper response

--- routes/ecom/modules/create-transaction.js
+++ routes/ecom/modules/create-transaction.js
@@ -65,8 +65,15 @@ module.exports = appSdk => {
           // https://dev.paghiper.com/reference#exemplos-pix
           const pixCode = createRequest.pix_code
           transaction.intermediator.transaction_code = pixCode.emv
-          transaction.payment_link = pixCode.pix_url
-          transaction.notes = `<img src="${transaction.qrcode_image_url}" ` +
+          const pixCodeUrls = ['pix_url', 'bacen_url', 'qrcode_image_url']
+          for (let i = 0; i < pixCodeUrls.length; i++) {
+            const pixUrl = pixCode[pixCodeUrls[i]]
+            if (pixUrl && pixUrl.startsWith('http')) {
+              transaction.payment_link = pixUrl
+              break
+            }
+          }
+          transaction.notes = `<img src="${pixCode.qrcode_image_url}" ` +
             'style="display:block;max-width:100%;margin:0 auto">'
         } else {
           const bankSlip = createRequest.bank_slip
